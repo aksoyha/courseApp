9dd42d8f40b056346f795fbe2799dda7b51add5d
Hatice Aksoy <Aksoysindar@outlook.com>
2026-01-12 15:10:01 +0300

    save

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.8/css/bootstrap.min.css" />
+    <title>Document</title>
+</head>
+<body>
+    <nav class="navbar navbar-expend-lg bg-primary navbar-dark">
+        <div class="container">
+            <a href="" class="navbar-brand">CourseApp</a>
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a href="" class="nav-link">Anasayfa</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                   <a href="" class="nav-link">Kurslar</a>
+                </li>
+            </ul>
+        </div>
+        <div class="container">
+            <div class="list-group">
+                <button type="button" class="list-group-item list-group-item-action active" aria-current="true">
+                          The current button
+                </button>
+                <button type="button" class="list-group-item list-group-item-action">A second button item</button>
+                <button type="button" class="list-group-item list-group-item-action">A third button item</button>
+                <button type="button" class="list-group-item list-group-item-action">A fourth button item</button>
+                <button type="button" class="list-group-item list-group-item-action" disabled>A disabled button item</button>
+            </div>
+            
+        </div>
+    </nav>
+    
+</body>
+</html>
