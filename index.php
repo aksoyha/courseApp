d3f7949598a46bd996cec127d2a3555dfe29534e
Hatice Aksoy <Aksoysindar@outlook.com>
2026-01-13 14:14:26 +0300

    save

--- index.php
+++ index.php
@@ -1,23 +1,26 @@
 <?php 
-$kategoriler=["Programlama","Web Geliştirme","Mobil Uygulamalar"];
+$kategoriler=["Programlama","Web Geliştirme","Mobil Uygulamalar","Ofis Uygulamaları"];
 $kurslar=[
     [
         "id"=>1,
-        "baslaik"=>"Web Geliştirme Kursu",
+        "baslik"=>"Web Geliştirme Kursu",
         "aciklama"=>"Gelişim destekli",
-        "resim"=>"prg.jpg"
+        "resim"=>"prg.jpg",
+        "onay"=>true
     ],
     [
         "id"=>2,
-        "baslaik"=>"JavaSript Geliştirme Kursu",
+        "baslik"=>"JavaSript Geliştirme Kursu",
         "aciklama"=>"Web için güzel bir kurs",
-        "resim"=>"js.jpg"
+        "resim"=>"js.jpg",
+        "onay"=>true
     ],
     [
         "id"=>3,
-        "baslaik"=>"Mobil Geliştirme Kursu",
+        "baslik"=>"Mobil Geliştirme Kursu",
         "aciklama"=>"Mobil dümyasına giriş yapın!!!!!",
-        "resim"=>"mbl.jpg"
+        "resim"=>"mbl.jpg",
+        "onay"=>true
     ]
 ];
 
@@ -47,72 +50,43 @@ $kurslar=[
             </ul>
         </div>
     </nav>
+    
     <div class="container my-3">
         <div class="row">
             <div class="col-3">
                 <div class="list-group">
-                    <?php  foreach($kategoriler as $kategori):?>
+                    <?php  for($i=0;$i<count($kategoriler); $i++):?>
                         
                         <a href="#" class="list-group-item list-group-item-action">
-                            <?php echo $kategori  ?>
+                            <?php echo $kategoriler[$i]  ?>
                         </a>
-                    <?php endforeach; ?>
+                    <?php endfor; ?>
                     <!-- <a href="#" class="list-group-item list-group-item-action">Web Geliştirme</a>
                     <a href="#" class="list-group-item list-group-item-action">Mobil Uygulama</a> -->
                 </div>
             </div>
             <div class="col-9">
-                <div class="card mb-3">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <img src="img/prg.jpg" alt="programming" class="img-fluid rounded-start">
-                        </div>
-                        <div class="cold-md-8">
-                            <div class="card-body">
-                                <h5 class="cart-title">Programlama</h5>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                     Fugiat enim accusantium, at ipsam doloribus labore consequatur 
-                                     velit a suscipit quos architecto nam animi. Quod, ex accusantium 
-                                     totam illum minus id.
-                                </p>
+                <?php foreach($kurslar as $kurs): ?>
+                    <?php if($kurs["onay"]== true): ?>
+                        <div class="card mb-3">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="img/<?php echo $kurs["resim"];?>" alt="programming" class="img-fluid rounded-start">
+                            </div>
+                            <div class="cold-md-8">
+                                <div class="card-body">
+                                    <h5 class="cart-title">
+                                        <?php echo $kurs["baslik"];?>
+                                    </h5>
+                                    <p>
+                                        <?php echo $kurs["aciklama"];?>
+                                    </p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div class="card mb-3">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <img src="img/js.jpg" alt="programming" class="img-fluid rounded-start">
                         </div>
-                        <div class="cold-md-8">
-                            <div class="card-body">
-                                <h5 class="cart-title">Programlama</h5>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                     Fugiat enim accusantium, at ipsam doloribus labore consequatur 
-                                     velit a suscipit quos architecto nam animi. Quod, ex accusantium 
-                                     totam illum minus id.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card mb-3">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <img src="img/mbl.jpg" alt="programming" class="img-fluid rounded-start">
-                        </div>
-                        <div class="cold-md-8">
-                            <div class="card-body">
-                                <h5 class="cart-title">Programlama</h5>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                     Fugiat enim accusantium, at ipsam doloribus labore consequatur 
-                                     velit a suscipit quos architecto nam animi. Quod, ex accusantium 
-                                     totam illum minus id.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                    <?php endif; ?>
+                <?php endforeach; ?>
             </div>
         </div>
             
